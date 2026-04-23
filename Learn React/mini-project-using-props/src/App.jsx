de112615ae1c0d7c27e23248c6e3f11748783fa9
Siddhartha Shakya <143714453+Sinmbf@users.noreply.github.com>
2026-04-23 21:52:54 +0545

    feat: create card

--- Learn React/mini-project-using-props/src/App.jsx
+++ Learn React/mini-project-using-props/src/App.jsx
@@ -1,5 +1,31 @@
+import Card from "./components/Card";
+import amazon from "./assets/amazon.jpg";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className="parent">
+      <Card
+        cardImg={amazon}
+        cardTitle="Amazon"
+        cardDescription="Senior UI/UX Designer"
+        cardTitleTime="2 days ago"
+        cardPrice="$120/hr"
+        location="Kathmandu,Nepal"
+        role="Part Timer"
+        level="Senior Level"
+      />
+      <Card
+        cardImg={amazon}
+        cardTitle="Google"
+        cardDescription="Senior UI/UX Designer"
+        cardTitleTime="3 days ago"
+        cardPrice="$140/hr"
+        location="Dallu,Nepal"
+        role="Part Timer"
+        level="Senior Level"
+      />
+    </div>
+  );
 };
 
 export default App;
